04439fa9e2b6e7b741ce20053cb970cea3919de9
miguel2887 <miguel.nieto280387@gmail.com>
2025-02-26 15:32:17 -0500

    numeros aleatorios

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
-import java.util.Scanner;
+import java.util.Random;
 
-public class Main {
-
+public class numeros_aleatorios {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
+        Random ale = new Random();
+        int minT = 2;
+        int maxT = 100;
+        int minN = 1;
+        int maxN = 6;
+        int sumaD1 = 0;
+        int sumaD2 = 0;
 
-        int n1, n2;
-        double raiz1, raiz2;
 
-        do{
-            System.out.println("ingrese el valor de n1");
-            n1 = teclado.nextInt();
-        } while (n1<=0);
-        do{
-            System.out.println("ingrese el valor de n2");
-            n2 = teclado.nextInt();
-        }while (n2<=0);
-        raiz1 = Math.sqrt(n1);
-        raiz2 = Math.sqrt(n2);
-
-        System.out.println("la raiz de n1 es" + raiz1);
-        System.out.println("la raiz de n2 es" + raiz2);
+        for (int i = 0; i < ale.nextInt(maxT - minT + 1) + minT; i++) {
+            int dado1 = ale.nextInt(maxN - minN + 1) + minN;
+            int dado2 = ale.nextInt(maxN - minN + 1) + minN;
+            sumaD1 += dado1;
+            sumaD2 += dado2;
+            System.out.println("Tiro " + (i + 1) + ": Dado 1 = " + dado1 + ", Dado 2 = " + dado2);
+        }
+        System.out.println("Suma total del Dado 1: " + sumaD1);
+        System.out.println("Suma total del Dado 2: " + sumaD2);
 
     }
 }
